0c6fbce9b1f0dc5d39278690afc6ba2044be1620
GoldStar184 <GoldStar184@worldpatch.dev>
2025-09-21 09:44:31 -0700

    `Data` class now can return drive motors

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Data.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/Data.java
@@ -8,20 +8,29 @@ public class Data {
     private Gamepad Gamepad1;
     private Gamepad Gamepad2;
     private HardwareMap hardwareMap;
+    DcMotor leftFrontDrive;
+    DcMotor rightFrontDrive;
+    DcMotor leftBackDrive;
+    DcMotor rightBackDrive;
 
     /**
      * Initialize all the data that can be passed to all the modules.
-     *
-     * The Drive motors are assumed to be all named
+     * The Drive motors should be passed in the order of left front, right front, left back, right back
      *
      * @param gamepad1 The First Gamepad, Required to be passed from an OP mode
      * @param gamepad2 The Second Gamepad, Required to be passed from an OP mode
      * @param hardwareMap The Hardware Map, Required to be passed from an OP mode
+     * @param leftFrontDrive The Left Front Drive Motor, Required to be passed from an OP mode
      * @author GoldStar184
      */
-    public void init(Gamepad gamepad1, Gamepad gamepad2, HardwareMap hardwareMap){
+    public void init(Gamepad gamepad1, Gamepad gamepad2, HardwareMap hardwareMap, DcMotor leftFrontDrive, DcMotor rightFrontDrive, DcMotor leftBackDrive, DcMotor rightBackDrive){
         Gamepad1 = gamepad1;
         Gamepad2 = gamepad2;
+        this.leftFrontDrive = leftFrontDrive;
+        this.rightFrontDrive = rightFrontDrive;
+        this.leftBackDrive = leftBackDrive;
+        this.rightBackDrive = rightBackDrive;
+
 
         this.hardwareMap = hardwareMap;
     }
@@ -38,5 +47,28 @@ public class Data {
     }
     public HardwareMap getHardwareMap(){
         return hardwareMap;
+    }
+    public DcMotor getDrive(String side1, String side2) throws IllegalArgumentException{
+
+        if (side1.equals("left")){
+            if(side2.equals("front")){
+                return leftFrontDrive;
+            }
+            else if(side2.equals("back")){
+                return leftBackDrive;
+            }
+        }
+        else if (side1.equals("right")){
+            if(side2.equals("front")){
+                return rightFrontDrive;
+            }
+            else if(side2.equals("back")){
+                return rightBackDrive;
+            }
+        }
+
+        throw new IllegalArgumentException("side1 must be left or right, side2 must be front or back");
+
+
     }
 }
