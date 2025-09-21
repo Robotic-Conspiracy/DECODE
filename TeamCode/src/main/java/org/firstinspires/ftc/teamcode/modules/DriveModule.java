9628858bad53c38edd58d87fd96ece1ea3cc1515
GoldStar184 <caichapmansharp@gmail.com>
2025-09-20 17:50:19 -0700

    [This commit message was AI generated]
    Added DriveModule.java
    
    This module controls the robot's mecanum drive using gamepad input. It initializes four DcMotors for the wheels and calculates power for each motor based on joystick inputs for forward movement, strafing, and rotation.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveModule.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode.modules;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Gamepad;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+public class DriveModule extends RobotModule{
+    private DcMotor leftFrontDrive;
+    private DcMotor rightFrontDrive;
+    private DcMotor leftBackDrive;
+    private DcMotor rightBackDrive;
+    private Gamepad gamepad1;
+    private HardwareMap hardwareMap;
+    double leftFrontPower;
+    double rightFrontPower;
+    double leftBackPower;
+    double rightBackPower;
+
+
+    public void run(){
+        gamepad1 = data.getGamepad(1);
+        hardwareMap = data.getHardwareMap();
+
+        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
+        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
+        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
+        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+
+        double forward = gamepad1.left_stick_y;
+        double strafe = gamepad1.left_stick_x;
+        double rotate = gamepad1.right_stick_x;
+
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontPower = (forward + strafe + rotate) / denominator;
+        rightFrontPower = (forward - strafe - rotate) / denominator;
+        leftBackPower = (forward - strafe + rotate) / denominator;
+        rightBackPower = (forward + strafe - rotate) / denominator;
+
+        leftFrontDrive.setPower(leftFrontPower);
+        rightFrontDrive.setPower(rightFrontPower);
+        leftBackDrive.setPower(leftBackPower);
+        rightBackDrive.setPower(rightBackPower);
+    }
+
+}
