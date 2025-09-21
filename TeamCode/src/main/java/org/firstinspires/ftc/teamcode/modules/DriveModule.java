3099435f25bf49ba266746d0bb14cf29051eebe0
GoldStar184 <GoldStar184@worldpatch.dev>
2025-09-21 09:49:21 -0700

    modify drive code to be better written and use `data.getDrive`

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveModule.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/modules/DriveModule.java
@@ -19,17 +19,22 @@ public class DriveModule extends RobotModule{
 
     public void run(){
         gamepad1 = data.getGamepad(1);
-        hardwareMap = data.getHardwareMap();
 
-        leftFrontDrive = hardwareMap.get(DcMotor.class, "left_front_drive");
-        rightFrontDrive = hardwareMap.get(DcMotor.class, "right_front_drive");
-        leftBackDrive = hardwareMap.get(DcMotor.class, "left_back_drive");
-        rightBackDrive = hardwareMap.get(DcMotor.class, "right_back_drive");
+        leftFrontDrive = data.getDrive("left", "front");
+        rightFrontDrive = data.getDrive("right", "front");
+        leftBackDrive = data.getDrive("left", "back");
+        rightBackDrive = data.getDrive("right", "back");
 
         double forward = gamepad1.left_stick_y;
         double strafe = gamepad1.left_stick_x;
         double rotate = gamepad1.right_stick_x;
 
+        //Comment below copied from sample code:
+
+        /* the denominator is the largest motor power (absolute value) or 1
+         * This ensures all the powers maintain the same ratio,
+         * but only if at least one is out of the range [-1, 1]
+         */
         double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
 
         leftFrontPower = (forward + strafe + rotate) / denominator;
