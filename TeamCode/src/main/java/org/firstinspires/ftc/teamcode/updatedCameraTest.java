0d9eb8adacb087e066baf504384f0adda8cdb8ac
geppp41-school <egep41414141@gmail.com>
2025-11-24 17:59:22 -0800

    got camera somewhat working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/updatedCameraTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/updatedCameraTest.java
@@ -2,10 +2,9 @@ package org.firstinspires.ftc.teamcode;
 
 import android.util.Size;
 
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-import org.firstinspires.ftc.robotcore.external.Telemetry;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.BuiltinCameraDirection;
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
@@ -16,25 +15,21 @@ import org.firstinspires.ftc.vision.VisionPortal;
 import org.firstinspires.ftc.vision.apriltag.AprilTagDetection;
 import org.firstinspires.ftc.vision.apriltag.AprilTagProcessor;
 
-import java.util.ArrayList;
-
 @TeleOp
-public class CameraTest extends LinearOpMode {
+public class updatedCameraTest extends OpMode {
     final boolean USING_WEBCAM = false;
     final BuiltinCameraDirection INTERNAL_CAM_DIR = BuiltinCameraDirection.BACK;
     final int RESOLUTION_WIDTH = 640;
     final int RESOLUTION_HEIGHT = 480;
     private AprilTagProcessor.Builder aprilTagProcessorBuilder = new AprilTagProcessor.Builder();
     private AprilTagProcessor aprilTagProcessor;
+    private VisionPortal portal;
 
     boolean lastX;
     int frameCount;
     long capReqTime;
-
-
     @Override
-    public void runOpMode() throws InterruptedException {
-        VisionPortal portal;
+    public void init() {
         aprilTagProcessorBuilder.setLensIntrinsics(811.775, 811.775, 364.462, 208.771);
         aprilTagProcessorBuilder.setCameraPose(new Position(DistanceUnit.MM, 285, 291, 45, System.currentTimeMillis()), new YawPitchRollAngles(AngleUnit.DEGREES, 0, 0, 90, System.currentTimeMillis()));
         aprilTagProcessorBuilder.setDrawTagOutline(true);
@@ -54,30 +49,24 @@ public class CameraTest extends LinearOpMode {
                     .addProcessor(aprilTagProcessor)
                     .build();
         }
-        while (!isStopRequested())
-        {
+        aprilTagProcessor.setPoseSolver(AprilTagProcessor.PoseSolver.APRILTAG_BUILTIN);
+    }
 
 
-            if (portal.getCameraState() == VisionPortal.CameraState.STREAMING){
-                if (aprilTagProcessor.getDetections().size() != 0){
-                    telemetry.addData("detections",
-                            GetAllDetectionIDs(aprilTagProcessor.getDetections()).toString()
-                            );
-                } else {
-                    telemetry.addData("detections", "none");
-                }
+    @Override
+    public void loop() {
+        if (portal.getCameraState() == VisionPortal.CameraState.STREAMING){
+            if (aprilTagProcessor.getDetections().size() != 0){
+                telemetry.addData("detections",
+                        aprilTagProcessor.getDetections().get(0).robotPose.getPosition().toString()
+                );
 
+
+            } else {
+                telemetry.addData("detections", "none");
             }
 
         }
         telemetry.update();
-    }
-
-    private ArrayList<Integer> GetAllDetectionIDs (ArrayList<AprilTagDetection> detections){
-        ArrayList<Integer> detection_ids = new ArrayList<>();
-        for(int i = 0; i < detections.size(); i++){
-            detection_ids.add(detections.get(i).id);
-        }
-        return detection_ids;
     }
 }
