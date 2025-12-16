7fc9b144f9a2cf861e765bd750d165fa265fabe3
samuel marten <147113447+PcPup@users.noreply.github.com>
2025-12-16 14:33:10 -0800

    Implement autonomous behavior for red team

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/back_line_auto_red.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/back_line_auto_red.java
@@ -1,1 +1,51 @@
-//placeholder
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+import kotlin.NotImplementedError;
+
+@Autonomous(name = "Red Shoot from back no sort")
+public class back_line_auto_red extends back_line_auto_main{
+    @Override
+    public void move() {
+        color = "Red";
+        double forward  = -0.25;
+        double strafe = 0;
+        double rotate = 0;
+
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontDrive.setPower((forward - strafe - rotate)/denominator);
+        leftBackDrive.setPower((forward + strafe - rotate)/denominator);
+        rightFrontDrive.setPower((forward + strafe + rotate)/denominator);
+        rightBackDrive.setPower((forward - strafe + rotate)/denominator);
+    }
+
+    @Override
+    public void rotate() {
+        double forward  = 0;
+        double strafe = 0;
+        double rotate = 0.15;
+
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontDrive.setPower((forward - strafe - rotate)/denominator);
+        leftBackDrive.setPower((forward + strafe - rotate)/denominator);
+        rightFrontDrive.setPower((forward + strafe + rotate)/denominator);
+        rightBackDrive.setPower((forward - strafe + rotate)/denominator);
+
+    }
+    @Override
+    public void strafe() {
+        double forward  = 0;
+        double strafe = 0.25;
+        double rotate = 0;
+
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontDrive.setPower((forward - strafe - rotate)/denominator);
+        leftBackDrive.setPower((forward + strafe - rotate)/denominator);
+        rightFrontDrive.setPower((forward + strafe + rotate)/denominator);
+        rightBackDrive.setPower((forward - strafe + rotate)/denominator);
+    }
+}
