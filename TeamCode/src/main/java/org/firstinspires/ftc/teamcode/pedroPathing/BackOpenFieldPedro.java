51721364eebebda90c0d053eb564ff9bd5e1992b
Aaron Marten <AaronRM@users.noreply.github.com>
2025-12-22 20:30:17 -0800

    Basic Pedro Path integration on Bot-2

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/BackOpenFieldPedro.java
@@ -0,0 +1,70 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.bylazar.configurables.PanelsConfigurables;
+import com.bylazar.configurables.annotations.IgnoreConfigurable;
+import com.bylazar.telemetry.PanelsTelemetry;
+import com.bylazar.telemetry.TelemetryManager;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.PathChain;
+import com.pedropathing.util.PoseHistory;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+@Autonomous
+public class BackOpenFieldPedro extends OpMode {
+    public static Follower follower;
+    @IgnoreConfigurable
+    static PoseHistory poseHistory;
+    @IgnoreConfigurable
+    static TelemetryManager telemetryM;
+
+    private final Pose startPose = new Pose(63.573330647946776, 8.043551310945293, Math.toRadians(90));
+    private final Pose endPose = new Pose(72, 72, Math.toRadians(141));
+    private PathChain path;
+    public static void draw() {
+        Drawing.drawDebug(follower);
+    }
+    public static void stopRobot() {
+        follower.startTeleopDrive(true);
+        follower.setTeleOpDrive(0,0,0,true);
+    }
+
+    @Override
+    public void init() {
+        if (follower == null) {
+            follower = Constants.createFollower(hardwareMap);
+            PanelsConfigurables.INSTANCE.refreshClass(this);
+        } else {
+            follower = Constants.createFollower(hardwareMap);
+        }
+
+        follower.setStartingPose(startPose);
+
+        poseHistory = follower.getPoseHistory();
+
+        telemetryM = PanelsTelemetry.INSTANCE.getTelemetry();
+
+        Drawing.init();
+
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        draw();
+
+
+
+    }
+    @Override
+    public void start() {
+        path = follower.pathBuilder()
+                .addPath(new BezierLine(startPose, endPose))
+                .setLinearHeadingInterpolation(startPose.getHeading(), endPose.getHeading())
+            .build();
+        follower.followPath(path);
+    }
+
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pedroPathing/Constants.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode.pedroPathing;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.follower.FollowerConstants;
+import com.pedropathing.ftc.FollowerBuilder;
+import com.pedropathing.ftc.drivetrains.MecanumConstants;
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
+import com.pedropathing.paths.PathConstraints;
+import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
+
+// Constants for 28139-A-RC (aka "Bot 2")
+public class Constants {
+    public static FollowerConstants followerConstants = new FollowerConstants()
+            .mass(9.07185)//must be in kg
+            .forwardZeroPowerAcceleration(-24.21937016587423)
+            .lateralZeroPowerAcceleration(-40.852422414063504);
+
+    public static PathConstraints pathConstraints = new PathConstraints(0.99, 100, 1, 1);
+
+    public static MecanumConstants driveConstants = new MecanumConstants()
+            .maxPower(1)
+            .rightFrontMotorName("front_right_drive")
+            .rightRearMotorName("back_right_drive")
+            .leftRearMotorName("back_left_drive")
+            .leftFrontMotorName("front_left_drive")
+            .leftFrontMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .leftRearMotorDirection(DcMotorSimple.Direction.REVERSE)
+            .rightFrontMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .rightRearMotorDirection(DcMotorSimple.Direction.FORWARD)
+            .xVelocity(41.7093505859375)
+            .yVelocity(33.222950131874384);
+
+
+    public static PinpointConstants localizerConstants = new PinpointConstants()
+            .forwardPodY(6)
+            .strafePodX(-4.5)
+            .distanceUnit(DistanceUnit.INCH)
+            .hardwareMapName("odometry")
+            .encoderResolution(GoBildaPinpointDriver.GoBildaOdometryPods.goBILDA_SWINGARM_POD)
+            .forwardEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD)
+            .strafeEncoderDirection(GoBildaPinpointDriver.EncoderDirection.FORWARD);
+    public static Follower createFollower(HardwareMap hardwareMap) {
+        return new FollowerBuilder(followerConstants, hardwareMap)
+                .pinpointLocalizer(localizerConstants)
+                .pathConstraints(pathConstraints)
+                .mecanumDrivetrain(driveConstants)
+                .build();
+    }
+}
