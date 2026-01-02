d68ac7e966d1abb46be1dd7877eadce0a8406b6c
geppp41 <egep41414141@gmail.com>
2026-01-02 11:54:30 -0800

    added autos to shoot from goal using old code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autos/back/The_Fourth_Auto_Red.java
@@ -0,0 +1,21 @@
+package org.firstinspires.ftc.teamcode.autos.back;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+
+@Autonomous(name = "Red goal")
+
+public class The_Fourth_Auto_Red extends The_Fourth_Auto{
+    @Override
+    void move() {
+        double forward  = 1;
+        double strafe = -1;
+        double rotate = 0;
+
+        double denominator = Math.max(Math.abs(forward) + Math.abs(strafe) + Math.abs(rotate), 1);
+
+        leftFrontDrive.setPower((forward - strafe - rotate)/denominator);
+        leftBackDrive.setPower((forward + strafe - rotate)/denominator);
+        rightFrontDrive.setPower((forward + strafe + rotate)/denominator);
+        rightBackDrive.setPower((forward - strafe + rotate)/denominator);
+    }
+}
