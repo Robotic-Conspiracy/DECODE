b7bdd5c0feddf123910ef6e145d7fd34fa9f2fb3
samuel marten <147113447+PcPup@users.noreply.github.com>
2026-01-09 09:00:01 -0800

    Refactor OpmodeConstants with new constants and values

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpmodeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpmodeConstants.java
@@ -1,13 +1,11 @@
 package org.firstinspires.ftc.teamcode;
 public class OpmodeConstants {
-
-    //TODO merge all constants accross files here
     public static final int backlineSpeed = 2240;
     public static final int midSpeed = 1940;
     public static final int goalSpeed = 1500;
-    public static final int midAngle = 48;
-    public static final int goalAngle = 75;
-    public static final int backlineAngle = 50;
+    public static final int midAngle = 48/360;
+    public static final int goalAngle = 75/360;
+    public static final int backlineAngle = 50/360;
 
     public static final int AutobacklineSpeed = 2240;
     public static final int AutomidSpeed = 1880;
@@ -15,6 +13,25 @@ public class OpmodeConstants {
     public static final int AutomidAngle = 47;
     public static final int AutogoalAngle = 75;
     public static final int AutobacklineAngle = 49;
+    
+    public static final int IntakeRampLaunchPos = 0.61;
+    public static final int IntakeRampIntakePos = 0.84;
+    public static final double FeedTimeSeconds = 0.15;
+
+    public static final double Launcher_P = 203;
+    public static final double Launcher_I = 1.001;
+    public static final double Launcher_D = 0.0015;
+    public static final double Launcher_F = 0.1;
+//hardware map names
+    public static final string IntakeName = "intake";
+    public static final string IntakeRampName = "intake ramp";
+    public static final string IntakeStoperName = "intake stopper";
+    public static final string AimServoName = "left twideler";
+    public static final string LeftFeederName = "left_feeder";
+    public static final string RightFeederName = "right_feeder";
+    public static final string LauncherName = "launcher";
+
+    
 
 
 
